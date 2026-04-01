6e427e0399769064baa9ef0f6130178d56eb07f7
lklinus2609 <lklinus0926@gmail.com>
2026-04-01 16:16:18 -0500

    Include configs_schema as standalone module in package

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="d4head_train",
     version="0.1.0",
     packages=find_packages(),
+    py_modules=["configs_schema"],
     python_requires=">=3.10",
     description="Adversarial full-face motion generation training pipeline for humanoid robots",
 )
